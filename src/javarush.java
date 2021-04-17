3a164862715909c2eae7a21ed471dc551adb521c
machukhinktato <tarabrinmv@gmail.com>
2021-04-17 14:00:28 +0300

    tasi done

---
+++ src/javarush.java
@@ -0,0 +1,12 @@
+public class javarush {
+//    типы комментирования
+    public static void main (String[] args)
+    {
+      /*
+      Сейчас мы выводим на экран фразу 'Amigo the Best'
+      */
+        System.out.print("Amigo ");
+        System.out.print("the "); // вот тут еще один комментарий
+        System.out.print("Best");
+    }
+}
