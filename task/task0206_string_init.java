f5ae3e0573a7849e1edd2e92a25bcb7af1721132
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-19 08:20:34 +0300

    Create task0206_string_init.java

---
+++ task/task0206_string_init.java
@@ -0,0 +1,18 @@
+/*
+В методе main объяви переменные word, phrase, line и text типа String.
+Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+Значениями могут быть любые строки.
+
+Требования:
+В методе main объяви четыре переменные типа String.
+Назови переменные именами word, phrase, line и text.
+Переменным сразу присвой значения.
+У всех переменных должны быть разные значения.
+*/
+
+public class task0206_string_init {
+
+    public static void main(String[] args) {
+        String word = "banana", phrase="None", line="new", text="not filled";
+    }
+}
