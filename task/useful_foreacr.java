ed273e2b8216dff78c3c6bac3c1be026213e5aa1
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-20 15:28:07 +0300

    Create useful_foreacr.java

---
+++ task/useful_foreacr.java
@@ -0,0 +1,12 @@
+public class ForExample {
+
+    public static void main(String[] args) {
+        String[] daysOfWeek =
+                { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+
+        for (String dayOfWeek : daysOfWeek) {
+            System.out.println(dayOfWeek);
+        }
+    }
+}
