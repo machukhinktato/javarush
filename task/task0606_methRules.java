8ec3d94ac4a423ad0573e1aa60c4ed2e9b1d63bc
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-23 11:27:12 +0300

    Create task0606_methRules.java

---
+++ task/task0606_methRules.java
@@ -0,0 +1,62 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+Неплохо было бы иметь один метод для решения различных задач. У тебя есть возможность написать такой.
+Создай еще 9 методов universalMethod(). В сумме их должно получиться 10.
+Параметры, которые они должны принимать, придумай сам.
+
+
+Требования:
+1. Программа должна содержать 10 методов с именем universalMethod.
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int integer) {
+
+    }
+
+    public static void universalMethod(String str) {
+
+    }
+
+    public static void universalMethod(double dbl) {
+
+    }
+
+    public static void universalMethod(float flt) {
+
+    }
+
+    public static void universalMethod(String str, int integer) {
+
+    }
+
+    public static void universalMethod(int[] banana) {
+
+    }
+
+    public static void universalMethod(String[] banana, int value) {
+
+    }
+
+    public static void universalMethod(int[][] banana, int value) {
+
+    }
+
+    public static void universalMethod(int[][][] banana, int value) {
+
+    }
+
+    public static void universalMethod(Scanner console) {
+
+    }
+
+}
