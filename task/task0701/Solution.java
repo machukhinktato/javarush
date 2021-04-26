897f8bade4335a2edc0df18747a31681845d5c12
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-26 09:44:01 +0300

    примитивный тип переменных

---
+++ task/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte one = 1;
+    short two = 2;
+    int three = 3;
+    long four = 4;
+    float five = 5.0F;
+    double six = 6.00;
+    boolean seven = true;
+    char eigth = '8';
+}
