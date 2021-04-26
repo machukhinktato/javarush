68bec5010002a3954e8a45bc12ccd0d3ddebfe66
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-26 13:04:16 +0300

    создание объектов

---
+++ task/task0712/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+Используя оператор new в методе main(), создай:
+
+10 объектов типа String;
+5 объектов типа int[] (массивов значений int);
+2 объекта типа Scanner.
+
+Требования:
+1. В методе main() должно создаваться 10 объектов типа String.
+2. В методе main() должно создаваться 5 объектов типа int[].
+3. В методе main() должно создаваться 2 объекта типа Scanner.
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a1 = new String("aha!");
+        String a2 = new String("aha!");
+        String a3 = new String("aha!");
+        String a4 = new String("aha!");
+        String a5 = new String("aha!");
+        String a6 = new String("aha!");
+        String a7 = new String("aha!");
+        String a8 = new String("aha!");
+        String a9 = new String("aha!");
+        String a0 = new String("aha!");
+        int[] b1 = new int[10];
+        int[] b2 = new int[10];
+        int[] b3 = new int[10];
+        int[] b4 = new int[10];
+        int[] b5 = new int[10];
+        Scanner console1 = new Scanner(System.in);
+        Scanner console2 = new Scanner(System.in);
+    }
+}
