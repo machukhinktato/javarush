7cc18a4100d7254c7e10f90a6a65b140f12bc01c
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-26 10:22:21 +0300

    максимальное число для примитивных integer digits

---
+++ task/task0702/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte bt = Byte.MAX_VALUE;
+    short shrt = Short.MAX_VALUE;
+    int intgr = Integer.MAX_VALUE;
+    public static long lng = Long.MAX_VALUE;
+
+//    public static void main(String[] args) {
+//        System.out.println(lng);
+//    }
+}
