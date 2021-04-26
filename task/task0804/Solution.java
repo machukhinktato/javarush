1b1b57aa4459e088776720902f8cce2a176d3ff0
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-26 15:48:29 +0300

    Рандомное число от 0 до 99

---
+++ task/task0804/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task08.task0804;
+
+/* 
+Randomizer
+В этой задаче тебе предстоит реализовать метод generateNumber(), который будет возвращать случайное число от 0 до 99.
+В методе generateNumber() используй метод Math.random().
+
+
+Требования:
+1. В классе Solution должен быть публичный статический метод generateNumber() с типом возвращаемого значения int.
+2. Реализуй метод generateNumber() согласно условию.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(generateNumber());
+    }
+
+    public static int generateNumber() {
+        return (int) (Math.random() * 99);
+    }
+}
