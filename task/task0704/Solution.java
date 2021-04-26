f15a3423c2d5c349cc3f03d3db37749ea1844d0f
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-26 10:46:30 +0300

    double  - exponenta

---
+++ task/task0704/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task07.task0704;
+
+/* 
+Экспонентная запись числа
+*/
+
+public class Solution {
+//    double earthDiameter = 12742.0;
+    double earthDiameter = 1.2742E+4;
+//    double lightSpeed = 299792458.0;
+    double lightSpeed = 2.99792458E+8;
+//    double uraniumAtomicMass = 238.0289;
+    double uraniumAtomicMass = 2.380289E+2;
+//    double averageBeeWeight = 0.085;
+    double averageBeeWeight = 8.5E-2;
+//    double javaDeveloperSalary = 10000.0;
+    double javaDeveloperSalary = 1E+4;
+}
