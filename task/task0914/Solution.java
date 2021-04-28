65c662b58092ee595a8ec0d595a558c24304c5f6
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-28 09:05:20 +0300

    Обновление пути, найти инексы, подстроку, заменить

---
+++ task/task0914/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task09.task0914;
+
+/* 
+Обновление пути
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        int start = path.indexOf("jdk");
+        int end = path.indexOf("/bin");
+        String word = path.substring(start, end);
+        path = path.replace(word, jdk);
+        return path;
+    }
+}
