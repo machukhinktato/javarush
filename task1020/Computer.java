d450e10bc1e9be626ae73b4ac0109cd3a26cd45e
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-28 18:43:39 +0300

    объявление многих классов в конструкторе одного

---
+++ task1020/Computer.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    public Computer(){
+        SystemUnit systemUnit = new SystemUnit();
+        Mouse mouse = new Mouse();
+        Keyboard keyboard = new Keyboard();
+        Monitor monitor = new Monitor();
+    }
+    public static void main(String[] args) {
+//        SystemUnit systemUnit = new SystemUnit();
+//        Mouse mouse = new Mouse();
+//        Keyboard keyboard = new Keyboard();
+//        Monitor monitor = new Monitor();
+        Computer pc = new Computer();
+    }
+}
