2eb91d486061e10cf0d150cb6d6c06fd52d2b404
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-04-29 21:21:47 +0300

    personalized arraylist COULD BE INTERESTING

---
+++ task1212/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task12.task1212;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CustomStringArrayList arrayList = new CustomStringArrayList();
+        for(int i = 0; i < 25; i++) {
+            arrayList.add("count" + i);
+        }
+//        for (int i = 0; i < 25; i++) {
+//            System.out.println(arrayList);
+//        }
+    }
+}
