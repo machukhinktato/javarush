5180a71ab08c302d8ce0f0fe2b49b95892d30e81
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-05-08 16:54:19 +0300

    Update Solution.java

--- task1521/Solution.java
+++ task1521/Solution.java
@@ -22,3 +22,31 @@ public class Solution {
         input.close();
     }
 }
+
+package com.javarush.task.pro.task15.task1521;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Временное сохранение файла
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        URL url = new URL(line);
+        InputStream input = url.openStream();
+//        Path tempFile = Files.createTempFile(null, null);
+        Path tempFile = Path.of("/home/machukhinktato/test.jpg");
+        Files.write(tempFile, input.readAllBytes());
+        input.close();
+    }
+}
+
+*/
