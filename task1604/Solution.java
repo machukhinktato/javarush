52e9c63adf3bbcdf91c298ea6ccf128a3817e774
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-05-11 10:13:55 +0300

    Calendar get Calendar.DAY_OF_WEEK

---
+++ task1604/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.pro.task16.task1604;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/* 
+День недели рождения твоего
+*/
+
+public class Solution {
+
+    static Calendar birthDate = new GregorianCalendar(1987, 9, 23);
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(Calendar calendar) {
+        String result = "";
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        switch (day) {
+            case 1:
+                return "воскресенье";
+            case 2:
+                return "понедельник";
+            case 3:
+                return "вторник";
+            case 4:
+                return "среда";
+            case 5:
+                return "четверг";
+            case 6:
+                return "пятница";
+            case 7:
+                return "суббота";
+        }
+        return result;
+        
+//        Date dob = calendar.getTime();
+//
+//        String s = switch (dob.getDay()) {
+//            case 1 -> "Понедельник";
+//            case 2 -> "Вторник";
+//            case 3 -> "Среда";
+//            case 4 -> "Четверг";
+//            case 5 -> "Пятница";
+//            case 6 -> "Суббота";
+//            case 0 -> "Воскресенье";
+//            default -> throw new IllegalStateException("Unexpected value: " + dob.getDay());
+//        };
+//        return s;
+    }
+}
