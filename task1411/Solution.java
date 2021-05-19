d8df13580a7942888f72ddbda4fa1da8be1a023f
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-05-19 19:52:56 +0300

    Add files via upload
    swtich case, reader, class init

--- task1411/Solution.java
+++ task1411/Solution.java
@@ -1,30 +1,55 @@
-package com.javarush.task.pro.task14.task1411;
+package com.javarush.task.task14.task1411;
 
-import java.io.FileNotFoundException;
-import java.nio.file.FileSystemException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /* 
-Распаковка исключений
+User, Loser, Coder and Proger
 */
 
 public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static final String FAILED_TO_READ = "Не удалось прочесть файл.";
-    public static final String FAILED_TO_WRITE = "Не удалось записать в файл.";
+        Person person = null;
+        //read values from keyboard here
+        String key;
+        while (true) {
+            key = reader.readLine();
+            boolean u = key.equals("user") || key.equals("loser") || key.equals("coder") || key.equals("proger");
+            if (!u) break;
 
-    public static FileManager fileManager = new FileManager();
-
-    public static void main(String[] args) {
-        try
-        {
-            fileManager.copyFile("book.txt", "book_final_copy.txt");
-            fileManager.copyFile("book_final_copy.txt", "book_last_copy.txt");
-        } catch (RuntimeException e) {
-            if (e.getCause() instanceof FileNotFoundException) {
-                System.out.println(FAILED_TO_READ);
-            } else if (e.getCause() instanceof FileSystemException) {
-                System.out.println(FAILED_TO_WRITE);
+            switch (key) {
+                case "user":
+                    person = new Person.User();
+                    break;
+                case "loser":
+                    person = new Person.Loser();
+                    break;
+                case "coder":
+                    person = new Person.Coder();
+                    break;
+                case "proger":
+                    person = new Person.Proger();
+                    break;
             }
+            doWork(person);
+        }
+    }
+
+    public static void doWork(Person person) {
+        if (person instanceof Person.User) {
+            Person.User user = (Person.User) person;
+            user.live();
+        } else if (person instanceof Person.Loser) {
+            Person.Loser loser = (Person.Loser) person;
+            loser.doNothing();
+        } else if (person instanceof Person.Coder) {
+            Person.Coder coder = (Person.Coder) person;
+            coder.writeCode();
+        } else if (person instanceof Person.Proger) {
+            Person.Proger proger = (Person.Proger) person;
+            proger.enjoy();
         }
     }
 }
