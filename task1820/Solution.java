c621219a62ce987b9542def283e4b0363ac05581
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-06-02 14:45:51 +0300

    bufferedreader filereader

--- task1820/Solution.java
+++ task1820/Solution.java
@@ -1,64 +1,28 @@
-package com.javarush.task.pro.task18.task1820;
+package com.javarush.task.task18.task1820;
 
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.io.*;
 
 /* 
-Самый дорогой автомобиль
+Округление чисел
 */
 
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
 
-    public static void main(String[] args) {
-        var tesla = Stream.of(
-                new Car("Model S", 94_490),
-                new Car("Model 3", 50_690),
-                new Car("Model X", 99_690),
-                new Car("Model Y", 65_000)
-        );
+        try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(fileName1));
+             PrintWriter printWriter = new PrintWriter(new FileWriter(fileName2))) {
 
-        var bmw = Stream.of(
-                new Car("X5", 110_000),
-                new Car("X3", 54_000),
-                new Car("X7", 143_000),
-                new Car("X6", 125_000)
-        );
-
-        Optional<Car> mostExpensiveCar = getMostExpensiveCar(tesla);
-        mostExpensiveCar.ifPresent(System.out::println);
-
-        Optional<Car> moreExpensiveCar = mostExpensiveCar.flatMap(car -> getMoreExpensiveCar(bmw, car));
-        moreExpensiveCar.ifPresent(System.out::println);
-    }
-
-    public static Optional<Car> getMostExpensiveCar(Stream<Car> cars) {
-        return cars.max((s1, s2) -> s1.getPrice() - s2.getPrice());
-    }
-
-    public static Optional<Car> getMoreExpensiveCar(Stream<Car> cars, Car mostExpensiveCar) {
-        return cars.filter(car -> car.getPrice() > mostExpensiveCar.getPrice()).findFirst();
-    }
-}
-
-class Car {
-    private String name;
-    private Integer price;
-
-    public Car(String name, Integer price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    @Override
-    public String toString() {
-        return "Автомобиль " + name + ", цена - " + price + " USD";
+            while (bufferedFileReader.ready()) {
+                String[] splittedLine = bufferedFileReader.readLine().split(" ");
+                for (String numberInString : splittedLine) {
+                    double number = Double.parseDouble(numberInString);
+                    long roundedNumber = Math.round(number);
+                    printWriter.print(roundedNumber + " ");
+                }
+            }
+        }
     }
 }
