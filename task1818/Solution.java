50153ff8f480e36cfa120f712ff62b83040be962
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-06-02 10:34:24 +0300

    write info to file from another and append 1 more

--- task1818/Solution.java
+++ task1818/Solution.java
@@ -1,49 +1,28 @@
-package com.javarush.task.pro.task18.task1818;
+package com.javarush.task.task18.task1818;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.stream.Stream;
+import java.io.*;
 
 /* 
-Просрочен ли товар?
+Два в одном
 */
 
 public class Solution {
-
-    public static void main(String[] args) {
-        var productStream = Stream.of(
-                new Product("Milk", LocalDate.of(2020, Month.MARCH, 12)),
-                new Product("Cheese", LocalDate.of(2020, Month.MARCH, 31)),
-                new Product("Buckwheat", LocalDate.of(2021, Month.MARCH, 1)),
-                new Product("Rice", LocalDate.of(2022, Month.MAY, 11)),
-                new Product("Wine", LocalDate.of(2023, Month.JANUARY, 10)),
-                new Product("Tea", LocalDate.of(2024, Month.AUGUST, 27)),
-                new Product("Salt", LocalDate.of(2025, Month.DECEMBER, 15))
-        );
-
-        String answer = hasExpiredProduct(productStream) ? "Yes" : "No";
-        System.out.println("Has stream expired products? " + answer);
-    }
-
-    public static boolean hasExpiredProduct(Stream<Product> productStream) {
-        return productStream.anyMatch(product -> product.getExpirationDate().isBefore(LocalDate.now()));
-    }
-}
-
-class Product {
-    private String name;
-    private LocalDate expirationDate;
-
-    public Product(String name, LocalDate expirationDate) {
-        this.name = name;
-        this.expirationDate = expirationDate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getExpirationDate() {
-        return expirationDate;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String firstFileName = reader.readLine();
+        String secondFileName = reader.readLine();
+        String thirdFileName = reader.readLine();
+        try (FileOutputStream fileOutputStream = new FileOutputStream(firstFileName, true);
+             FileInputStream fileInputStream1 = new FileInputStream(secondFileName);
+             FileInputStream fileInputStream2 = new FileInputStream(thirdFileName)) {
+            while (fileInputStream1.available() > 0) {
+                int data = fileInputStream1.read();
+                fileOutputStream.write(data);
+            }
+            while (fileInputStream2.available() > 0) {
+                int data = fileInputStream2.read();
+                fileOutputStream.write(data);
+            }
+        }
     }
 }
